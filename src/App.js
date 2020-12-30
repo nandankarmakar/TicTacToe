55ab396f7c46b11f44d6ed6ebf0d6f8f8fbd3599
Nandan Karmakar <nandan.karmakar739@gmail.com>
2020-12-30 15:22:10 +0530

    Few Changes

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ const App = () => {
 
   return (
     <Container className="p-5">
+    <h1 className='text-center text-white text-info'>Play TicTacToe, Have fun!!</h1>
       <ToastContainer position="bottom-center" />
       <Row>
         <Col md={6} className="offset-md-3">
